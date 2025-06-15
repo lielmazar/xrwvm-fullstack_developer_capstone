71f1681a65eb42f485f466527c9f56120a7bd3c0
lielmazar <30670189+lielmazar@users.noreply.github.com>
2025-06-16 00:07:24 +0300

    Update settings.py to pass linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.' + \ 
+        'django.contrib.auth.' + \
         'password_validation.UserAttributeSimilarityValidator',
     },
     {
